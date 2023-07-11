13b7d27b455faf8c23e78eac9a96f946a58c6843
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 16:00:47 +0300

    Added a ProducesResponseType thing

--- CloudCustomers.API/Controllers/UsersController.cs
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -15,9 +15,17 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet(Name = "GetUsers")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetUsersEndpoint()
     {
         var users = await _usersService.GetAllUsers();
+
+        if (users == null)
+        {
+            return NotFound();
+        }
+
         return Ok(users);
     }
 }
