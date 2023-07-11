87b942a1a274a81ca622b8d66dc5d3024cd73bf8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 15:07:57 +0300

    Finished writing the logic of the seconfd test

--- CloudCustomers.Tests/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers.Tests/Systems/Controllers/TestUsersController.cs
@@ -6,6 +6,7 @@ using FluentAssertions;
 using System.Threading.Tasks;
 using Moq;
 using CloudCustomers.API.Services;
+using CloudCustomers.API.Models;
 
 public class TestUsersController
 {
@@ -21,13 +22,17 @@ public class TestUsersController
     // }
 
     [Fact]
-    public async Task GetUsersEndpoint_OnSuccess_InvokeUserService()
+    public async Task GetUsersEndpoint_OnSuccess_InvokeUserServiceExactlyOnce()
     {
         // Arrange
-        var mockUserService = new Mock<IUserService>();
-        var sut = new UsersController(mockUserService.Object);
+        var mockUsersService = new Mock<IUserService>();
+        mockUsersService
+                .Setup(service => service.GetAllUsers())
+                .ReturnsAsync(new List<User>());
+        var sut = new UsersController(mockUsersService.Object);
         // Act
-        var result = await sut.GetUsersEndpoint() as OkObjectResult;
+        var result = await sut.GetUsersEndpoint();
         // Assert
+        mockUsersService.Verify(service => service.GetAllUsers(), Times.Once);
     }
 }
