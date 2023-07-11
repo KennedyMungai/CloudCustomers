c898d2b0f83cb4efde2447264f0d78f9687a661b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 14:47:31 +0300

    Implemented a new test and commented out the first one

--- CloudCustomers.Tests/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers.Tests/Systems/Controllers/TestUsersController.cs
@@ -4,17 +4,30 @@ using CloudCustomers.API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using FluentAssertions;
 using System.Threading.Tasks;
+using Moq;
+using CloudCustomers.API.Services;
 
 public class TestUsersController
 {
+    // [Fact]
+    // public async Task GetUsersEndpoint_OnSuccess_ReturnsStatusCode200OK()
+    // {
+    //     // Arrange
+    //     var sut = new UsersController();
+    //     // Act
+    //     var result = await sut.GetUsersEndpoint() as OkObjectResult;
+    //     // Assert
+    //     result.StatusCode.Should().Be(200);
+    // }
+
     [Fact]
-    public async Task GetUsersEndpoint_OnSuccess_ReturnsStatusCode200OK()
+    public async Task GetUsersEndpoint_OnSuccess_InvokeUserService()
     {
         // Arrange
-        var sut = new UsersController();
+        var mockUserService = new Mock<IUserService>();
+        var sut = new UsersController(mockUserService.Object);
         // Act
-        var result = (OkObjectResult)await sut.GetUsersEndpoint();
+        var result = await sut.GetUsersEndpoint() as OkObjectResult;
         // Assert
-        result.StatusCode.Should().Be(200);
     }
 }
