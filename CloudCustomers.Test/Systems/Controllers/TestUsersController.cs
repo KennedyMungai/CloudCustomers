0489e984a000b482ca61e6f047549ada7e894ebf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 14:05:52 +0300

    Created the first test

--- CloudCustomers.Test/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers.Test/Systems/Controllers/TestUsersController.cs
@@ -1,10 +1,17 @@
 namespace CloudCustomers.Test.Systems.Controllers;
 
+using CloudCustomers.API.Controllers;
+
 public class TestUsersController
 {
     [Fact]
-    public void Test1()
+    public void GetUsersEndpoint_OnSuccess_ReturnsStatusCode200OK()
     {
-
+        // Arrange
+        var sut = new UsersController();
+        // Act
+        var result = sut.GetUsersEndpoint();
+        // Assert
+        result.StatusCode.Should().Be(200);
     }
 }
