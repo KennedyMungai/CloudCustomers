cbef2ce1930c8237fc620216fafc26dbdfba9b13
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 14:11:13 +0300

    Added some namespaces to get rid of the errors

--- CloudCustomers.Test/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers.Test/Systems/Controllers/TestUsersController.cs
@@ -1,16 +1,18 @@
 namespace CloudCustomers.Test.Systems.Controllers;
 
 using CloudCustomers.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
 
 public class TestUsersController
 {
     [Fact]
-    public void GetUsersEndpoint_OnSuccess_ReturnsStatusCode200OK()
+    public async Task GetUsersEndpoint_OnSuccess_ReturnsStatusCode200OK()
     {
         // Arrange
         var sut = new UsersController();
         // Act
-        var result = sut.GetUsersEndpoint();
+        var result = (OkObjectResult)await sut.GetUsersEndpoint();
         // Assert
         result.StatusCode.Should().Be(200);
     }
