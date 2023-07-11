bb553b2a5c8a5b8e8d9f5617bf253725afa9318e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 12:51:53 +0300

    Repurposed the Westhser controller logic for the UsersController

--- CloudCustomers.API/Controllers/UsersController.cs
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -6,11 +6,6 @@ namespace CloudCustomers.API.Controllers;
 [Route("[controller]")]
 public class UsersController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<UsersController> _logger;
 
     public UsersController(ILogger<UsersController> logger)
@@ -18,15 +13,9 @@ public class UsersController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetUsers")]
+    public asyncTask<IActionResult> GetUsersEndpoint()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return null;
     }
 }
