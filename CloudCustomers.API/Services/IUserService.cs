7ef0dd53c70216a1ca0a3746b2dfcd498820f715
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-11 15:02:13 +0300

    IMplemented the GetAllUsers service

--- CloudCustomers.API/Services/IUserService.cs
+++ CloudCustomers.API/Services/IUserService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CloudCustomers.API.Models;
 
 namespace CloudCustomers.API.Services
 {
     public interface IUserService
     {
-        
+        Task<List<User>> GetAllUsers();
     }
 }
--- CloudCustomers.API/Services/UserService.cs
+++ CloudCustomers.API/Services/UserService.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CloudCustomers.API.Models;
 
 namespace CloudCustomers.API.Services
 {
     public class UserService : IUserService
     {
-
+        public Task<List<User>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
