b5c7d8bc9361b7c72664dfb641ec9183cd43bcbe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-12 09:24:38 +0300

    Created the UserFixture file

---
+++ CloudCustomers.Tests/Fixtures/UsersFixture.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudCustomers.API.Models;
+
+namespace CloudCustomers.Tests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() =>
+        {
+            new ()
+            {
+                new User()
+        {
+
+        }
+    }
+}
+    }
+}
