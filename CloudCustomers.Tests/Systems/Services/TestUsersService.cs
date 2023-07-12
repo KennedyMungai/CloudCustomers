fe2d2b834bbb74e4339d4eaf59c8e90ea725f454
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-12 10:33:44 +0300

    Added a http mock to the test logic

--- CloudCustomers.Tests/Systems/Services/TestUsersService.cs
+++ CloudCustomers.Tests/Systems/Services/TestUsersService.cs
@@ -1,4 +1,7 @@
+using CloudCustomers.API.Models;
 using CloudCustomers.API.Services;
+using CloudCustomers.Tests.Fixtures;
+using CloudCustomers.Tests.Helpers;
 
 namespace CloudCustomers.Tests.Systems.Services;
 
@@ -9,7 +12,10 @@ public class TestUserService
     public async Task GetAllUsers_WhenCalled_InvokesHttpHetRequest()
     {
         // Arrange
-        var sut = new UserService();
+        var expectedResponse = UsersFixture.GetTestUsers();
+        var handlerMock = MockHttpMessageHandlers<User>.SetupBasicGetResourceList(expectedResponse: expectedResponse);
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UserService(httpClient);
         // Act
         await sut.GetAllUsers();
         // Assert
