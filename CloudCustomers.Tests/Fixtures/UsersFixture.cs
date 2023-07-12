73386c18f0364d65f0312b3b0ac8642f48278eb1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-12 09:35:11 +0300

    Created a static list containing three users

--- CloudCustomers.Tests/Fixtures/UsersFixture.cs
+++ CloudCustomers.Tests/Fixtures/UsersFixture.cs
@@ -1,22 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CloudCustomers.API.Models;
 
-namespace CloudCustomers.Tests.Fixtures
-{
-    public static class UsersFixture
-    {
-        public static List<User> GetTestUsers() =>
-        {
-            new ()
-            {
-                new User()
-        {
+namespace CloudCustomers.Tests.Fixtures;
 
+
+public static class UsersFixture
+{
+    public static List<User> GetTestUsers() => new()
+    {
+        new User
+        {
+            Name = "Test User 1",
+            Email = "test@user.com",
+            Address = new Address
+            {
+                Street = "123 Main St",
+                City = "Los Angeles",
+                ZipCode = "12345"
+            }
+        },
+        new User
+        {
+            Name = "Test User 2",
+            Email = "test2@user.com",
+            Address = new Address
+            {
+                Street = "1234 Main St",
+                City = "Los Angeles 4",
+                ZipCode = "123465"
+            }
+        },
+        new User
+        {
+            Name = "Test User 3",
+            Email = "test3@user.com",
+            Address = new Address
+            {
+                Street = "12345 Main St",
+                City = "Los Angeles 45",
+                ZipCode = "1234656"
+            }
         }
-    }
-}
-    }
+    };
 }
