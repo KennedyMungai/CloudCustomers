6514e516e393c72031744c769330f5c2809ea1b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-12 10:03:50 +0300

    Set up a dependency injection for the HttpClient for the UserService

--- CloudCustomers.API/Services/UserService.cs
+++ CloudCustomers.API/Services/UserService.cs
@@ -8,6 +8,13 @@ namespace CloudCustomers.API.Services
 {
     public class UserService : IUserService
     {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<List<User>> GetAllUsers()
         {
             throw new NotImplementedException();
